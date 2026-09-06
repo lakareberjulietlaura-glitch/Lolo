89db213cd53af414b0151cef82fd997fe785f92d
lakareberjulietlaura-glitch <lakareberjulietlaura@gmail.com>
2026-09-06 21:28:45 +0300

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,26 @@
+number1 = int(input("Enter the first number:"))
+number2 = float(input("Enter the second num,ber"))
+
+#Addition
+sum = number1 + number2
+print(sum)
+
+number1 = 8
+number2 = 4
+
+#Subbraction
+difference = number1 - number2
+print(difference)
+
+#Modulus
+modulus = number1#number2
+print(modulus)
+
+#Concatenation
+
+print(f"The first numberis {number1}, and the second number is")
+
+firstName = "Juliet"
+secondName = "Pa Daddy"
+
+print(f"My first name is {firstName} and my second name is {secondName}")
