fccca1b37dbba3a01b734057e74f3bda20db4a97
lakareberjulietlaura-glitch <lakareberjulietlaura@gmail.com>
2026-09-14 08:14:25 +0300

    Add files via upload

---
+++ Lolo.py
@@ -0,0 +1,83 @@
+from stanfordkarel import *
+print('karel library imported succwssfully!')
+
+def main():
+    turn_left()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    turn_right()
+    move()
+    move()
+    turn_right()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+
+
+def turn_left():
+      move()
+      move()
+      move()
+          
+
+def turn_right():
+        turn_left()
+        turn_left()
+        turn_left()
+
+
+def finish():
+       move() 
+                     
+        
+            
+
+if __name__ == "__main__":
+    run_karel_program()    
+  
+
+
+
+   
+    
